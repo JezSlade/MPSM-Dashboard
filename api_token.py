0b0bf8acf8796afccc39681c4e06f87de20627aa
JezSlade <slade.rbd@gmail.com>
2025-05-13 17:45:25 -0400

    nhgfb

--- api_token.py
+++ api_token.py
@@ -5,12 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_token():
-    auth_url = os.getenv("BASE_URL") + "/Login/Authenticate"
+    url = os.getenv("BASE_URL") + "/Login/Authenticate"
     payload = {
         "Username": os.getenv("USERNAME"),
         "Password": os.getenv("PASSWORD"),
         "DealerCode": os.getenv("DEALER_CODE")
     }
-    res = requests.post(auth_url, json=payload)
-    res.raise_for_status()
-    return res.json().get("Token")
+
+    try:
+        res = requests.post(url, json=payload)
+        return res.json()["Token"]
+    except Exception as e:
+        print("Error getting token:")
+        print("Status:", res.status_code)
+        print("Text:", res.text)
+        raise
