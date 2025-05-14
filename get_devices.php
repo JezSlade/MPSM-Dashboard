9b2597335cce2efa7c2870638d319c834f4fe636
JezSlade <slade.rbd@gmail.com>
2025-05-14 08:46:21 -0400

    revert

--- get_devices.php
+++ get_devices.php
@@ -1,12 +1,37 @@
 <?php
-$python = "/usr/bin/python3";
-$script = "/home/resolut7/public_html/mpsm.resolutionsbydesign.us/mpsm/get_devices.py";
+require_once 'working_token.php';
 
-$input = file_get_contents("php://input");
-$data = json_decode($input, true);
-$customerId = escapeshellarg($data["CustomerId"]);
+$token = get_token();
+$apiUrl = 'https://api.abassetmanagement.com/api3/CustomerDashboard/Devices';
 
-header("Content-Type: application/json");
-$output = shell_exec("$python $script $customerId 2>&1");
-echo $output;
-?>
+$headers = [
+  "Authorization: Bearer $token",
+  "Content-Type: application/json"
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+  http_response_code(500);
+  echo json_encode(["error" => curl_error($ch)]);
+  exit;
+}
+
+curl_close($ch);
+$devices = json_decode($response, true);
+
+$simpleList = array_map(function ($device) {
+  return [
+    'AssetNumber' => $device['AssetNumber'] ?? '',
+    'IpAddress'   => $device['IpAddress'] ?? '',
+    'Model'       => $device['Model'] ?? ''
+  ];
+}, $devices);
+
+header('Content-Type: application/json');
+echo json_encode($simpleList);
