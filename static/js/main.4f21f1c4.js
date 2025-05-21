90869812804b5d3305ac6f7c72ac5407a94a6720
JezSlade <slade.rbd@gmail.com>
2025-05-21 15:19:54 -0400

    Auto build and deploy 2025-05-21 15:19:53

--- static/js/main.4f21f1c4.js
+++
@@ -1,6 +0,0 @@
-/*! For brevity, the main JS bundle content is typically very large and minified.
-    Here’s a placeholder; in your actual build this file contains all React app logic.
-    You need to use your React build tool (like Create React App or Vite) to produce this file.
-    
-    Please build locally or request me to provide the unminified source code for development.
-*/
