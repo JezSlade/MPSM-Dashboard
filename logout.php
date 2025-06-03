930fdba64b3723560814563e21d77918097833e4
Jez Slade <jez.slade@gmail.com>
2025-06-03 16:47:29 -0400

    Refactor logout and authentication logic to use dynamic path detection for redirection

--- logout.php
+++ logout.php
@@ -4,5 +4,8 @@
 session_start();
 session_unset();
 session_destroy();
-header('Location: /login.php');
+
+$base = dirname($_SERVER['SCRIPT_NAME']);
+$redirectPath = rtrim($base, '/') . '/login.php';
+header("Location: {$redirectPath}");
 exit;
