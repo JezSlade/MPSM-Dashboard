2ce778e5997802e6b362944015d3ab4d09a3829f
Jez Slade <jez.slade@gmail.com>
2025-06-12 14:28:29 -0400

    feat: Add navigation structure to enhance user interface and improve accessibility

---
+++ includes/navigation.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <?php foreach ($available_views as $slug => $title): ?>
+            <li class="<?= ($slug == $current_view_slug) ? 'active' : '' ?>">
+                <a href="?view=<?= urlencode($slug) ?>"><?= sanitize_html($title) ?></a>
+            </li>
+        <?php endforeach; ?>
+        <li><a href="?logout=true">Logout</a></li>
+    </ul>
+</nav>
+<main></main>
