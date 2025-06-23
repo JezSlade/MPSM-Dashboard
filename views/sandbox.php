efd289010e370b93da9b2d4abce8c3abd627fdea
Jez <slade.rbd@gmail.com>
2025-06-23 08:43:24 -0400

    mbnv

--- views/sandbox.php
+++ views/sandbox.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * Sandbox View
  * ------------------------------------------------------------------
@@ -24,7 +25,7 @@ ini_set('log_errors', '1');
 ini_set('error_log', __DIR__ . '/logs/debug.log');
 // ------------------------------------------------------------------
 
-declare(strict_types=1);
+
 
 // ------------------------------------------------------------------
 // 1. List of active cards for the current sandbox session.
