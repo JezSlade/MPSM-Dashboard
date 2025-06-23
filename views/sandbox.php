ebbcb0751419a4b96b6d203e2525da26e9f7b0ee
Jez <slade.rbd@gmail.com>
2025-06-23 08:57:30 -0400

    csx

--- views/sandbox.php
+++ views/sandbox.php
@@ -22,8 +22,7 @@ declare(strict_types=1);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('log_errors', '1');
-ini_set('error_log', __DIR__ . '/logs/debug.log');
-// ------------------------------------------------------------------
+ini_set('error_log', __DIR__ . '/../logs/debug.log');   // ← fixed path// ------------------------------------------------------------------
 
 
 
