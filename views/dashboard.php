6aa0b253e5695dba1327a345aec4d13672740923
Jez Slade <jez.slade@gmail.com>
2025-06-24 17:11:45 -0400

    Refactor dashboard.php to improve HTML structure and organization

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html><html lang="en"><head>
+  <meta charset="UTF-8"><title>Dashboard</title>
+</head><body>
+<?php include __DIR__.'/../includes/header.php';?>
+<?php include __DIR__.'/../includes/navigation.php';?>
+<main><?php include __DIR__.'/../cards/CustomersCard.php';?></main>
+<?php include __DIR__.'/../includes/footer.php';?>
+</body></html>
 <?php
 // /views/dashboard.php
 
