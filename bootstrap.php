2ffe7b193bea2063a90c3a5911c40a32f7834551
Jez Slade <jez.slade@gmail.com>
2025-07-02 08:53:42 -0400

    feat: Add dashboard settings file and define constant for its path

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,4 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 require_once 'helpers.php';
 // Load env vars, db, config, etc. as needed
+define('DASHBOARD_SETTINGS_FILE', __DIR__ . '/data/dashboard_settings.json');
